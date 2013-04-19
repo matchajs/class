979170c582f6f8e1dfb2e039c54c81ebb1983a55
kidney <kidneyleung@gmail.com>
2013-04-19 13:20:35 +0800

    create test spec

---
+++ test/class-spec.js
@@ -0,0 +1,29 @@
+define(function(require) {
+    var Class = require('../src/class');
+
+    describe('Class', function() {
+        it('Class.create', function() {
+
+        });
+
+        it('Extends', function() {
+
+        });
+
+        it('Implements', function() {
+
+        });
+
+        it('Class.extend', function() {
+
+        });
+
+        it('SubClass.extend', function() {
+
+        });
+
+        it('SubClass.implement', function() {
+
+        });
+    });
+});
