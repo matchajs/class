7f6fe2c88ce65b4981f20d890936f6411e6d8765
kidney <kidneyleung@gmail.com>
2013-06-17 14:11:41 +0800

    edit test

--- test/class-spec.js
+++ test/class-spec.js
@@ -1,9 +1,26 @@
 define(function(require) {
-    var Class = require('../src/class');
+    var expect = require('expect');
+    var Class = require('class');
 
     describe('Class', function() {
         it('Class.create', function() {
+            var Pig = Class.create({
+                Statics: {
+                    title: 'A1'
+                },
+                initialize: function(name) {
+                    this.name = name;
+                },
 
+                talk: function() {
+                    return ('I am ' + this.name);
+                },
+
+                pigType: 'normalPig'
+            });
+            var MyPig = new Pig('Super Pig');
+            expect(MyPig.pigType).to.equal('normalPig');
+            expect(MyPig.talk).to.equal('I am Super Pig');
         });
 
         it('Extends', function() {
